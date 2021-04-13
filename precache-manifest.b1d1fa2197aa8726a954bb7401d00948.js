2939d6151694b696cadc317878d8d2fe8792fc02
AleksYan <oyankovskyi@gmail.com>
2021-04-13 22:22:23 +0200

    Updates

---
+++ precache-manifest.b1d1fa2197aa8726a954bb7401d00948.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ac6581502b22d36bac7685c545ba7a",
+    "url": "/react-wxcc-widget/index.html"
+  },
+  {
+    "revision": "d409df65244d0ba00bb1",
+    "url": "/react-wxcc-widget/react-widget-starter.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/react-wxcc-widget/static/media/getFetch.013f0b90.cjs"
+  }
+]);
